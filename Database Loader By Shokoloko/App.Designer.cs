8bf9448740e77887b0dcede14c071f713f6ee42a
Your Name <you@example.com>
2022-08-25 19:14:59 +0300

    Update
    Added FileWatcher
    Fixed stop search button

---
+++ Database Loader By Shokoloko/App.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Database_Loader_By_Shokoloko
+{
+    partial class DatabaseLoader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ResultsTxtBox = new System.Windows.Forms.TextBox();
+            this.KeywordsTxtBox = new System.Windows.Forms.TextBox();
+            this.KeywordsLbl = new System.Windows.Forms.Label();
+            this.SearchBtn = new System.Windows.Forms.Button();
+            this.StopSearchBtn = new System.Windows.Forms.Button();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.LoadDatabaseBtn = new System.Windows.Forms.Button();
+            this.DatabaseSelectMenu = new System.Windows.Forms.ComboBox();
+            this.CreditsLbl = new System.Windows.Forms.Label();
+            this.SeperatorLbl = new System.Windows.Forms.Label();
+            this.SeperatorTxtBox = new System.Windows.Forms.TextBox();
+            this.ExactResultsCheckBox = new System.Windows.Forms.CheckBox();
+            this.ReloadDatabasesBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ResultsTxtBox
+            // 
+            this.ResultsTxtBox.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.ResultsTxtBox.Location = new System.Drawing.Point(18, 236);
+            this.ResultsTxtBox.MaxLength = 999999999;
+            this.ResultsTxtBox.Multiline = true;
+            this.ResultsTxtBox.Name = "ResultsTxtBox";
+            this.ResultsTxtBox.ReadOnly = true;
+            this.ResultsTxtBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ResultsTxtBox.Size = new System.Drawing.Size(770, 202);
+            this.ResultsTxtBox.TabIndex = 1;
+            this.ResultsTxtBox.TextChanged += new System.EventHandler(this.ResultsTxtBox_TextChanged);
+            // 
+            // KeywordsTxtBox
+            // 
+            this.KeywordsTxtBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.KeywordsTxtBox.Location = new System.Drawing.Point(18, 41);
+            this.KeywordsTxtBox.Multiline = true;
+            this.KeywordsTxtBox.Name = "KeywordsTxtBox";
+            this.KeywordsTxtBox.Size = new System.Drawing.Size(371, 189);
+            this.KeywordsTxtBox.TabIndex = 5;
+            this.KeywordsTxtBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeywordsTxtBox_KeyDown);
+            // 
+            // KeywordsLbl
+            // 
+            this.KeywordsLbl.AutoSize = true;
+            this.KeywordsLbl.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.KeywordsLbl.Location = new System.Drawing.Point(14, 18);
+            this.KeywordsLbl.Name = "KeywordsLbl";
+            this.KeywordsLbl.Size = new System.Drawing.Size(73, 20);
+            this.KeywordsLbl.TabIndex = 4;
+            this.KeywordsLbl.Text = "Keywords";
+            // 
+            // SearchBtn
+            // 
+            this.SearchBtn.Font = new System.Drawing.Font("Arial", 12F);
+            this.SearchBtn.Location = new System.Drawing.Point(395, 41);
+            this.SearchBtn.Name = "SearchBtn";
+            this.SearchBtn.Size = new System.Drawing.Size(125, 54);
+            this.SearchBtn.TabIndex = 6;
+            this.SearchBtn.Text = "Search";
+            this.SearchBtn.UseVisualStyleBackColor = true;
+            this.SearchBtn.Click += new System.EventHandler(this.SearchBtn_Click);
+            // 
+            // StopSearchBtn
+            // 
+            this.StopSearchBtn.Enabled = false;
+            this.StopSearchBtn.Font = new System.Drawing.Font("Arial", 12F);
+            this.StopSearchBtn.Location = new System.Drawing.Point(529, 41);
+            this.StopSearchBtn.Name = "StopSearchBtn";
+            this.StopSearchBtn.Size = new System.Drawing.Size(125, 54);
+            this.StopSearchBtn.TabIndex = 8;
+            this.StopSearchBtn.Text = "Stop Search";
+            this.StopSearchBtn.UseVisualStyleBackColor = true;
+            this.StopSearchBtn.Click += new System.EventHandler(this.StopSearchBtn_Click);
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.Enabled = false;
+            this.ClearBtn.Font = new System.Drawing.Font("Arial", 12F);
+            this.ClearBtn.Location = new System.Drawing.Point(663, 41);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(125, 54);
+            this.ClearBtn.TabIndex = 9;
+            this.ClearBtn.Text = "Clear";
+            this.ClearBtn.UseVisualStyleBackColor = true;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // LoadDatabaseBtn
+            // 
+            this.LoadDatabaseBtn.Font = new System.Drawing.Font("Arial", 12F);
+            this.LoadDatabaseBtn.Location = new System.Drawing.Point(395, 112);
+            this.LoadDatabaseBtn.Name = "LoadDatabaseBtn";
+            this.LoadDatabaseBtn.Size = new System.Drawing.Size(125, 63);
+            this.LoadDatabaseBtn.TabIndex = 11;
+            this.LoadDatabaseBtn.Text = "Load Database";
+            this.LoadDatabaseBtn.UseVisualStyleBackColor = true;
+            this.LoadDatabaseBtn.Click += new System.EventHandler(this.LoadDatabaseBtn_Click);
+            // 
+            // DatabaseSelectMenu
+            // 
+            this.DatabaseSelectMenu.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.DatabaseSelectMenu.DropDownWidth = 125;
+            this.DatabaseSelectMenu.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.DatabaseSelectMenu.FormattingEnabled = true;
+            this.DatabaseSelectMenu.Location = new System.Drawing.Point(529, 152);
+            this.DatabaseSelectMenu.Name = "DatabaseSelectMenu";
+            this.DatabaseSelectMenu.Size = new System.Drawing.Size(125, 23);
+            this.DatabaseSelectMenu.TabIndex = 10;
+            // 
+            // CreditsLbl
+            // 
+            this.CreditsLbl.AutoSize = true;
+            this.CreditsLbl.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.CreditsLbl.Location = new System.Drawing.Point(656, 112);
+            this.CreditsLbl.Name = "CreditsLbl";
+            this.CreditsLbl.Size = new System.Drawing.Size(142, 57);
+            this.CreditsLbl.TabIndex = 17;
+            this.CreditsLbl.Text = "Credits:\r\nCracked: shokoloko\r\nTelegram: shokoloko1";
+            // 
+            // SeperatorLbl
+            // 
+            this.SeperatorLbl.AutoSize = true;
+            this.SeperatorLbl.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.SeperatorLbl.Location = new System.Drawing.Point(398, 182);
+            this.SeperatorLbl.Name = "SeperatorLbl";
+            this.SeperatorLbl.Size = new System.Drawing.Size(77, 20);
+            this.SeperatorLbl.TabIndex = 14;
+            this.SeperatorLbl.Text = "Seperator:";
+            // 
+            // SeperatorTxtBox
+            // 
+            this.SeperatorTxtBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.SeperatorTxtBox.Location = new System.Drawing.Point(481, 183);
+            this.SeperatorTxtBox.MaxLength = 1;
+            this.SeperatorTxtBox.Name = "SeperatorTxtBox";
+            this.SeperatorTxtBox.Size = new System.Drawing.Size(39, 23);
+            this.SeperatorTxtBox.TabIndex = 13;
+            this.SeperatorTxtBox.Text = ":";
+            // 
+            // ExactResultsCheckBox
+            // 
+            this.ExactResultsCheckBox.AutoSize = true;
+            this.ExactResultsCheckBox.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.ExactResultsCheckBox.Checked = true;
+            this.ExactResultsCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ExactResultsCheckBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.ExactResultsCheckBox.Location = new System.Drawing.Point(398, 211);
+            this.ExactResultsCheckBox.Name = "ExactResultsCheckBox";
+            this.ExactResultsCheckBox.Size = new System.Drawing.Size(94, 19);
+            this.ExactResultsCheckBox.TabIndex = 16;
+            this.ExactResultsCheckBox.Text = "Exact Results";
+            this.ExactResultsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ReloadDatabasesBtn
+            // 
+            this.ReloadDatabasesBtn.Font = new System.Drawing.Font("Arial", 9.75F);
+            this.ReloadDatabasesBtn.Location = new System.Drawing.Point(529, 112);
+            this.ReloadDatabasesBtn.Name = "ReloadDatabasesBtn";
+            this.ReloadDatabasesBtn.Size = new System.Drawing.Size(125, 34);
+            this.ReloadDatabasesBtn.TabIndex = 12;
+            this.ReloadDatabasesBtn.Text = "Reload Databases";
+            this.ReloadDatabasesBtn.UseVisualStyleBackColor = true;
+            this.ReloadDatabasesBtn.Click += new System.EventHandler(this.ReloadDatabasesBtn_Click);
+            // 
+            // DatabaseLoader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ExactResultsCheckBox);
+            this.Controls.Add(this.SeperatorTxtBox);
+            this.Controls.Add(this.SeperatorLbl);
+            this.Controls.Add(this.CreditsLbl);
+            this.Controls.Add(this.DatabaseSelectMenu);
+            this.Controls.Add(this.ReloadDatabasesBtn);
+            this.Controls.Add(this.LoadDatabaseBtn);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.StopSearchBtn);
+            this.Controls.Add(this.SearchBtn);
+            this.Controls.Add(this.KeywordsLbl);
+            this.Controls.Add(this.KeywordsTxtBox);
+            this.Controls.Add(this.ResultsTxtBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DatabaseLoader";
+            this.Text = "Database Loader By Shokoloko";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ResultsTxtBox;
+        private System.Windows.Forms.TextBox KeywordsTxtBox;
+        private System.Windows.Forms.Label KeywordsLbl;
+        private System.Windows.Forms.Button SearchBtn;
+        private System.Windows.Forms.Button StopSearchBtn;
+        private System.Windows.Forms.Button ClearBtn;
+        private System.Windows.Forms.Button LoadDatabaseBtn;
+        private System.Windows.Forms.ComboBox DatabaseSelectMenu;
+        private System.Windows.Forms.Label CreditsLbl;
+        private System.Windows.Forms.Label SeperatorLbl;
+        private System.Windows.Forms.TextBox SeperatorTxtBox;
+        private System.Windows.Forms.CheckBox ExactResultsCheckBox;
+        private System.Windows.Forms.Button ReloadDatabasesBtn;
+    }
+}
+
---
+++ Database Loader By Shokoloko/App.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace Database_Loader_By_Shokoloko
+{
+    public partial class DatabaseLoader : Form
+    {
+        List<List<string>> Identities = new List<List<string>>();
+        List<string> Keywords = new List<string>();
+        string Seperator;
+        Thread SearchProcess;
+        public DatabaseLoader()
+        {
+            InitializeComponent();
+            if (!Directory.Exists("data"))
+            {
+                Directory.CreateDirectory("data");
+            }
+
+            #region watcher
+
+            this.BuildDirWatcher();
+
+            #endregion watcher
+
+            if (Directory.GetFiles("data").Length == 0)
+            {
+                Console.WriteLine("Add databases to the data folder");
+            }
+            foreach (string file in Directory.GetFiles("data"))
+            {
+                this.DatabaseSelectMenu.Items.Add(file.Split(new char[] { '\\', '/' }, StringSplitOptions.None).Last());
+            }
+            if (this.DatabaseSelectMenu.Items.Count > 0)
+            {
+                this.DatabaseSelectMenu.SelectedIndex = 0;
+            }
+            this.Seperator = this.SeperatorTxtBox.Text;
+        }
+        private void BuildDirWatcher()
+        {
+            // Another problem to fix: I need to detect when someone renames the data folder and rename it back to "data"
+            FileSystemWatcher watcher = new FileSystemWatcher($"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}\\data");
+            watcher.NotifyFilter = NotifyFilters.Attributes
+                                    | NotifyFilters.CreationTime
+                                    | NotifyFilters.DirectoryName
+                                    | NotifyFilters.FileName
+                                    | NotifyFilters.LastAccess
+                                    | NotifyFilters.LastWrite
+                                    | NotifyFilters.Security
+                                    | NotifyFilters.Size;
+
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+            watcher.Created += ((a, b) => ReloadDatabasesBtn_Click(new object { }, new EventArgs()));
+            watcher.Changed += ((a, b) => ReloadDatabasesBtn_Click(new object { }, new EventArgs()));
+            watcher.Deleted += ((a, b) => ReloadDatabasesBtn_Click(new object { }, new EventArgs()));
+            watcher.Renamed += ((a, b) => {
+                if (Directory.Exists(b.FullPath) && b.FullPath == watcher.Path)
+                {
+                    Thread.Sleep(200);
+                    Directory.Move(b.FullPath, b.OldFullPath);
+                }
+                else
+                {
+                    ReloadDatabasesBtn_Click(new object { }, new EventArgs());
+                }
+            });
+            watcher.Error += ((a, b) =>
+            {
+                // I did it cuz the latency of creating / updating directories... very weird, if you have any solution please make a pull
+                // If you have any other solution instead of this shitty recursion make a pull
+                Thread.Sleep(800);
+                var spin = new ConsoleSpinner();
+                Console.WriteLine("Creating new data folder...");
+                while (!Directory.Exists(watcher.Path))
+                {
+                    Directory.CreateDirectory(watcher.Path);
+                    spin.Turn();
+                }
+                Console.SetCursorPosition(Console.WindowLeft, Console.CursorTop);
+                Console.WriteLine("Created data folder");
+                Thread.Sleep(200);
+                BuildDirWatcher();
+            });
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SearchBtn_Click(object sender, EventArgs e)
+        {
+            this.Keywords = this.KeywordsTxtBox.Text.Split('\n').Select(x => x.Trim()).Where(x => x != String.Empty && x != Environment.NewLine).ToList();
+            if (this.Keywords.Count == 0)
+            {
+                MessageBox.Show("You are not using any keywords, insert your keywords.", "Error", MessageBoxButtons.OK);
+                return;
+            }
+            if (this.Identities.Count == 0)
+            {
+                MessageBox.Show("You didn't load any database.", "Error", MessageBoxButtons.OK);
+                return;
+            }
+            this.SearchBtn.Enabled = false;
+            this.StopSearchBtn.Enabled = true;
+            this.SearchProcess = new Thread(new ThreadStart(() =>
+            {
+                new Task(() =>
+                {
+                    var spin = new ConsoleSpinner();
+                    Console.Write("Searching....");
+                    while (true)
+                    {
+                        spin.Turn();
+                    }
+                }).Start();
+                List<List<string>> FoundIdentities;
+                if(this.ExactResultsCheckBox.Checked)
+                {
+                    FoundIdentities = this.Identities.Where(x => this.Keywords.All(y => x.Any(z => z == y))).ToList();
+                }
+                else
+                {
+                    FoundIdentities = this.Identities.Where(x => this.Keywords.All(y => x.Any(z => z.Contains(y)))).ToList();
+                }
+
+                Console.SetCursorPosition(Console.WindowLeft, Console.CursorTop);
+                Console.WriteLine($"Found {FoundIdentities.Count} results.");
+
+                this.StopSearchBtn.Enabled = false;
+                this.SearchBtn.Enabled = true;
+                this.SearchProcess = null;
+
+                if (FoundIdentities.Count == 0)
+                {
+                    MessageBox.Show("Found 0 matches.", "Error", MessageBoxButtons.OK);
+                    return;
+                }
+                
+                this.ResultsTxtBox.Text += String.Join(Environment.NewLine, FoundIdentities.Select(x => String.Join(", ", x))) + Environment.NewLine;
+                
+
+            }));
+            this.SearchProcess.Start();
+        }
+
+        private void StopSearchBtn_Click(object sender, EventArgs e)
+        {
+            Console.SetCursorPosition(Console.WindowLeft, Console.CursorTop);
+            Console.WriteLine("Stopping the search...");
+            try
+            {
+                this.SearchProcess?.Abort();
+            }
+            catch
+            {
+
+            }
+            this.SearchProcess = null;
+            this.StopSearchBtn.Enabled = false;
+            this.SearchBtn.Enabled = true;
+        }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            this.ResultsTxtBox.Text = String.Empty;
+            this.ClearBtn.Enabled = false;
+        }
+
+        private void LoadDatabaseBtn_Click(object sender, EventArgs e)
+        {
+            if (DatabaseSelectMenu.Text == String.Empty)
+            {
+                MessageBox.Show("No database selected.", "Error", MessageBoxButtons.OK);
+                return;
+            }
+            if (this.SeperatorTxtBox.Text == String.Empty)
+            {
+                this.SeperatorTxtBox.Text = ":";
+            }
+            
+            this.Seperator = this.SeperatorTxtBox.Text;
+
+            Console.WriteLine($"Loading database - {DatabaseSelectMenu.Text}");
+            this.LoadDatabaseBtn.Enabled = false;
+            this.SeperatorTxtBox.Enabled = false;
+            new Thread(new ThreadStart(() =>
+            {
+                try
+                {
+                    new Task(() =>
+                    {
+                        var spin = new ConsoleSpinner();
+                        Console.Write("Loading....");
+                        while (true)
+                        {
+                            spin.Turn();
+                        }
+                    }).Start();
+                    this.Identities.Clear();
+                    using (FileStream fs = File.Open($"data/{DatabaseSelectMenu.Text}", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                    using (BufferedStream bs = new BufferedStream(fs))
+                    
+                    using (StreamReader sr = new StreamReader(bs))
+                    {
+                        this.Identities = sr.ReadToEnd().Split('\n').Select(x => x.Trim().Split(this.Seperator.ToCharArray()).Where(y => y != String.Empty && y != "\r").ToList()).ToList();
+                    }
+                    if (this.Identities.Count == 0)
+                    {
+                        Console.WriteLine("Load your database");
+                    }
+                    Console.SetCursorPosition(Console.WindowLeft, Console.CursorTop);
+                    Console.WriteLine($"Loaded database - {DatabaseSelectMenu.Text} with {this.Identities.Count} lines");
+                    this.LoadDatabaseBtn.Enabled = true;
+                    this.SeperatorTxtBox.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            })).Start();
+        }
+
+        private void ReloadDatabasesBtn_Click(object sender, EventArgs e)
+        {
+            this.DatabaseSelectMenu.Items.Clear();
+            if (!Directory.Exists("data"))
+            {
+                Directory.CreateDirectory("data");
+            }
+            foreach (string file in Directory.GetFiles("data", "*", SearchOption.AllDirectories))
+            {
+                this.DatabaseSelectMenu.Items.Add(file.Split(new char[] { '\\', '/' }, StringSplitOptions.None).Last());
+            }
+            if (this.DatabaseSelectMenu.Items.Count > 0)
+            {
+                this.DatabaseSelectMenu.SelectedIndex = 0;
+            }
+        }
+        private void KeywordsTxtBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.V)
+            {
+                try
+                {
+                    Clipboard.SetText(Clipboard.GetText().Replace("\r", "").Replace("\n", Environment.NewLine));
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+        private void ResultsTxtBox_TextChanged(object sender, EventArgs e)
+        {
+            if (this.ResultsTxtBox.Text.Length > 0)
+            {
+                this.ClearBtn.Enabled = true;
+            }
+        }
+    }
+}
